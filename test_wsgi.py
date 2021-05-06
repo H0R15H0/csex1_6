1e55c79b351895659353fe406d4024215a57ee4c
H0R15H0 <shohori13@gmail.com>
2021-05-06 14:06:30 +0900

    マッチしないルートにアクセスがきた時にNotFoundを返すようにした

--- test_wsgi.py
+++ test_wsgi.py
@@ -26,70 +26,77 @@ con.close()
 def application(environ,start_response):
     # HTML（共通ヘッダ部分）
     html = '<html lang="ja">\n' \
-           '<head>\n' \
-           '<meta charset="UTF-8">\n' \
-           '<title>WSGI テスト</title>\n' \
-           '<link rel="stylesheet" href="default.css">\n' \
-           '</head>\n'
+        '<head>\n' \
+        '<meta charset="UTF-8">\n' \
+        '<title>WSGI テスト</title>\n' \
+        '<link rel="stylesheet" href="default.css">\n' \
+        '</head>\n'
 
-    # フォームデータを取得
-    form = cgi.FieldStorage(environ=environ,keep_blank_values=True)
-    if ('v1' not in form) or ('v2' not in form):
-        # 入力フォームの内容が空の場合（初めてページを開いた場合も含む）
+    if environ['PATH_INFO'] == '/':
+        # フォームデータを取得
+        form = cgi.FieldStorage(environ=environ,keep_blank_values=True)
+        if ('v1' not in form) or ('v2' not in form):
+            # 入力フォームの内容が空の場合（初めてページを開いた場合も含む）
 
-        # HTML（入力フォーム部分）
-        html += '<body>\n' \
-                '<div class="form1">\n' \
-                '<form>\n' \
-                '学生番号（整数） <input type="text" name="v1"><br>\n' \
-                '氏名　（文字列） <input type="text" name="v2"><br>\n' \
-                '<input type="submit" value="登録">\n' \
-                '</form>\n' \
-                '</div>\n' \
-                '</body>\n'
+            # HTML（入力フォーム部分）
+            html += '<body>\n' \
+                    '<div class="form1">\n' \
+                    '<form>\n' \
+                    '学生番号（整数） <input type="text" name="v1"><br>\n' \
+                    '氏名　（文字列） <input type="text" name="v2"><br>\n' \
+                    '<input type="submit" value="登録">\n' \
+                    '</form>\n' \
+                    '</div>\n' \
+                    '</body>\n'
+        else:
+            # 入力フォームの内容が空でない場合
+
+            # フォームデータから各フィールド値を取得
+            v1 = form.getvalue("v1", "0")
+            v2 = form.getvalue("v2", "0")
+
+            # データベース接続とカーソル生成
+            con = sqlite3.connect(dbname)
+            cur = con.cursor()
+            con.text_factory = str
+
+            # SQL文（insert）の作成と実行
+            sql = 'insert into users (id, name) values (?,?)'
+            cur.execute(sql, (int(v1),v2))
+            con.commit()
+
+            # SQL文（select）の作成
+            sql = 'select * from users'
+
+            # SQL文の実行とその結果のHTML形式への変換
+            html += '<body>\n' \
+                    '<div class="ol1">\n' \
+                    '<ol>\n'
+            for row in cur.execute(sql):
+                html += '<li>' + str(row[0]) + ',' + row[1] + '</li>\n'
+            html += '</ol>\n' \
+                    '</div>\n' \
+                    '<a href="/">登録ページに戻る</a>\n' \
+                    '</body>\n'
+
+            # カーソルと接続を閉じる
+            cur.close()
+            con.close()
+
+
+        html += '</html>\n'
+        html = html.encode('utf-8')
+
+        # レスポンス
+        start_response('200 OK', [('Content-Type', 'text/html; charset=utf-8'),
+            ('Content-Length', str(len(html))) ])
+        return [html]
     else:
-        # 入力フォームの内容が空でない場合
-
-        # フォームデータから各フィールド値を取得
-        v1 = form.getvalue("v1", "0")
-        v2 = form.getvalue("v2", "0")
-
-        # データベース接続とカーソル生成
-        con = sqlite3.connect(dbname)
-        cur = con.cursor()
-        con.text_factory = str
-
-        # SQL文（insert）の作成と実行
-        sql = 'insert into users (id, name) values (?,?)'
-        cur.execute(sql, (int(v1),v2))
-        con.commit()
-
-        # SQL文（select）の作成
-        sql = 'select * from users'
-
-        # SQL文の実行とその結果のHTML形式への変換
-        html += '<body>\n' \
-                '<div class="ol1">\n' \
-                '<ol>\n'
-        for row in cur.execute(sql):
-            html += '<li>' + str(row[0]) + ',' + row[1] + '</li>\n'
-        html += '</ol>\n' \
-                '</div>\n' \
-                '<a href="/">登録ページに戻る</a>\n' \
-                '</body>\n'
-
-        # カーソルと接続を閉じる
-        cur.close()
-        con.close()
-
-
-    html += '</html>\n'
-    html = html.encode('utf-8')
-
-    # レスポンス
-    start_response('200 OK', [('Content-Type', 'text/html; charset=utf-8'),
-        ('Content-Length', str(len(html))) ])
-    return [html]
+        html += '<h1>404 Not Found</h1>'
+        html = html.encode('utf-8')
+        start_response('404 Not Found', [('Content-type', 'text/html; charset=utf-8'), 
+            ('Content-Length', str(len(html))) ])
+        return [html]
 
 
 # リファレンスWEBサーバを起動
