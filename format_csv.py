04216637b8eb4db2d0710e2b11ac1a0cd1341fa0
H0R15H0 <shohori13@gmail.com>
2021-05-06 15:15:39 +0900

    idに重複があるからindexでPKを当てた。

--- format_csv.py
+++ format_csv.py
@@ -8,9 +8,10 @@ def format_aozora():
             authors = csv.writer(f)
             with open('./aozora.csv') as f:
                 reader = csv.reader(f)
-                for row in reader:
-                    id, title, class_name, published_at, author_id, author = row[0], row[1], row[2], row[3], row[5], row[6]
-                    books.writerow([title, class_name, published_at, author_id])
+                for i, row in enumerate(reader):
+                    i += 1
+                    id, title, class_name, published_at, author_id, author = i, row[1], row[2], row[3], row[5], row[6]
+                    books.writerow([id, title, class_name, published_at, author_id])
                     if author_id not in author_ids:
                         author_ids.append(author_id)
                         authors.writerow([author_id, author])
