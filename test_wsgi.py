ac58b24e10f6ff8a0bb1fc4623fc1eefd3a43ba3
H0R15H0 <shohori13@gmail.com>
2021-05-10 17:55:05 +0900

    コメントを追加

--- test_wsgi.py
+++ test_wsgi.py
@@ -25,7 +25,7 @@ dbname = 'database.db'
 SQL.initialize_db()
 
 def application(environ,start_response):
-    # HTML（共通ヘッダ部分）
+    # HTML（共通ヘッダ部分）NavBarもここで作成
     html = '<html lang="ja">\n' \
         '<head>\n' \
         '<meta charset="UTF-8">\n' \
@@ -44,10 +44,10 @@ def application(environ,start_response):
         '</nav> \n' 
 
     if environ['PATH_INFO'] == '/':
+        # タイトルの設定
         html = html.format(title='ようこそ！')
-
+        # HTMLを読み込む
         html += templates.index_html.html_body()
-
         html += '</html>\n'
         html = html.encode('utf-8')
 
@@ -57,10 +57,10 @@ def application(environ,start_response):
         return [html]
     
     elif environ['PATH_INFO'] == '/mypage':
+        # タイトルの設定
         html = html.format(title='登録情報の変更')
-
+        # HTMLを読み込む
         html += templates.mypage_html.html_body()
-
         html += '</html>\n'
         html = html.encode('utf-8')
 
@@ -70,20 +70,23 @@ def application(environ,start_response):
         return [html]
         
     elif environ['PATH_INFO'] == '/books':
+        # タイトルの設定
+        html = html.format(title='本一覧')
+        # クエリパラメーター(book_title)の取得
         query_params = urllib.parse.parse_qs(environ["QUERY_STRING"])
         search_book_title = ''
         if len(query_params):
             search_book_title = query_params['book_title'][0]
-        html = html.format(title='本一覧')
 
         # SQL文の実行とその結果のHTML形式への変換
         query = 'select books.id, books.title, books.published_at, authors.id, authors.name from books JOIN authors ON books.author_id=authors.id '
+        # クエリパラメーター(book_title)が存在している時、部分一致検索。存在していない時、全体検索。
         query += f'where books.title LIKE "%{search_book_title}%";' if len(search_book_title) else ';'
         query = SQL(query)
         results = query.execute()
 
+        # HTMLを読み込む
         html += templates.books_index_html.html_body(results)
-
         html += '</html>\n'
         html = html.encode('utf-8')
 
@@ -93,12 +96,15 @@ def application(environ,start_response):
         return [html]
 
     elif environ['REQUEST_METHOD'] == 'POST' and environ['PATH_INFO'] == '/users':
+        # POSTリクエストボディの取得
         body = environ['wsgi.input'].read(int(environ.get('CONTENT_LENGTH', 0))).decode('utf-8')
         form = urllib.parse.parse_qs(body)
 
+        # すでにUserが存在していれば取得
         query = SQL('select * from users where id = {};'.format(form['student_id'][0]))
         user = query.execute()
 
+        # ↑で存在していたらUPDATE処理、存在していなかったらCREATE処理
         if user:
             query = SQL('update users set name="{}" where id={}'.format(form['name'][0], user[0]))
             query.execute()
@@ -106,21 +112,30 @@ def application(environ,start_response):
             query = SQL('insert into users(id, name) values ({},"{}")'.format(int(form['student_id'][0]), form['name'][0]))
             query.execute()
 
+        # /books 本の一覧ページにリダイレクトさせる。
         start_response('301 Moved', [('Location','/books')])
         return ''
 
+    # /books/{id}/users_books_comments のルーティング
     elif environ['REQUEST_METHOD'] == 'POST' and re.compile('/books/(?P<book_id>\d+)/users_books_comments').fullmatch(environ['PATH_INFO']).groupdict()["book_id"]:
+        # 正規表現で{id}の取得
         book_id = re.compile('/books/(?P<book_id>\d+)/users_books_comments').match(environ['PATH_INFO']).groupdict()["book_id"]
+        # POSTリクエストボディの取得
         body = environ['wsgi.input'].read(int(environ.get('CONTENT_LENGTH', 0))).decode('utf-8')
         form = urllib.parse.parse_qs(body)
+        # コメントの作成
         query = SQL('insert into users_books_comments(user_id, book_id, message) values ({},{},"{}")'.format(int(form['user_student_id'][0]), book_id, form['message'][0]))
         query.execute()
 
+        # /books/{id} 本の詳細ページにリダイレクト
         start_response('301 Moved', [('Location','/books/{}'.format(book_id))])
         return ''
 
+    # /books/{id} のルーティング
     elif re.compile('/books/(?P<book_id>\d+)').fullmatch(environ['PATH_INFO']).groupdict()["book_id"]:
+        # タイトルの設定
         html = html.format(title='本の詳細')
+        # 正規表現で{id}の取得
         book_id = re.compile('/books/(?P<book_id>\d+)').match(environ['PATH_INFO']).groupdict()["book_id"]
 
         # SQL文の実行とその結果のHTML形式への変換
@@ -132,7 +147,7 @@ def application(environ,start_response):
         if type(comments) is tuple:
             comments = [comments] 
 
-
+        # HTMLを読み込む
         html += templates.books_show_html.html_body(book, comments)
         html += '</html>\n'
         html = html.encode('utf-8')
@@ -142,6 +157,7 @@ def application(environ,start_response):
             ('Content-Length', str(len(html))) ])
         return [html]
 
+    # どのルーティングにもマッチしなかった時、'404 Not Found'を返す
     else:
         html = html.format(title='Not found')
         html += '<h1>404 Not Found</h1>'
